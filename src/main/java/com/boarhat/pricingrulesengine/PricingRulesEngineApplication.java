e20d16dca297c3c93fb55374d8c9de13983efae9
Laurent FREDERICK <laurent.frederick@icloud.com>
2025-11-22 09:13:05 +0100

    init commit

---
+++ src/main/java/com/boarhat/pricingrulesengine/PricingRulesEngineApplication.java
@@ -0,0 +1,13 @@
+package com.boarhat.pricingrulesengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingRulesEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricingRulesEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boarhat/pricingrulesengine/PricingRulesEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boarhat.pricingrulesengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingRulesEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
